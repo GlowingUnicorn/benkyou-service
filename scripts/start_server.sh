5ce35c34855270617517043027b7f1a5e97072c9
Lora-Ronco-Kendem, Louis <louis.lora-ronco-kendem@jpmchase.com>
2023-06-21 22:44:39 +0200

    chore: update script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,14 +1,13 @@
 echo "Starting the server"
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install 16;
+sudo -s
 
 cd /home/ec2-user/benkyou-service
-sudo npm install
+
+npm install
 npm run start:build
-sudo systemctl restart node-api.service
+
+exit
+
+pm2 restart all
