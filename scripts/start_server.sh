1dc0cd725c9a3cc6d5525c39080c6691d783f482
Lora-Ronco-Kendem, Louis <louis.lora-ronco-kendem@jpmchase.com>
2023-06-21 22:51:26 +0200

    chore: add nvm installation to start script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,9 @@
 echo "Starting the server"
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
 nvm install 16;
 sudo -s
 
