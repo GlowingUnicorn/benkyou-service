77d16cc00578994668ae9fd870093d113acac79a
Lora-Ronco-Kendem, Louis <louis.lora-ronco-kendem@jpmchase.com>
2023-06-21 12:10:27 +0200

    chore: add nvm registration to deploy hook scripts

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,4 +1,12 @@
 echo "Stopping the server"
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 systemctl stop node-api.service
 sudo rm -rf /home/ec2-user/benkyou-service
